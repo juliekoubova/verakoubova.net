cd9f3b9d7f704a19bd4f9b358d3666845e7bedd9
Julie Koubová <julie@koubova.net>
2020-04-10 23:46:56 +0200

    bibliography

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,17 @@
+const yaml = require('js-yaml');
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addDataExtension('yaml', text => yaml.safeLoad(text))
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addFilter('lang', function (value) {
+    if (typeof value === 'object' && value) {
+      if (this.ctx && this.ctx.lang && this.ctx.lang in value)  {
+        return value[this.ctx.lang];
+      }
+      const keys = Object.keys(value);
+      if (keys && keys.length !== 0) {
+        return value[keys[0]];
+      }
+    }
+    return value;
+  });
 }
--- _data/site.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  languages: [
-    { label: "Česky", code: 'cz' },
-    { label: "Deutsch", code: 'de' },
-  ],
-  mail: 'verkou@verakoubova.net'
-}
