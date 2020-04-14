b77d1dca2092cc476cc2c3fdd21114034b18cb55
Julie Koubová <julie@koubova.net>
2020-04-15 00:27:29 +0200

    script and styles

--- _assets/main.ts
+++ _assets/main.ts
@@ -1,10 +1,10 @@
-import { vekApp } from './vek-app'
+import { vekApp } from './js/vek-app'
 // import { CanScrollController } from './can-scroll'
 // import { ScrollToController } from './scroll-to'
-import { setupHasScrolled } from './has-scrolled'
-import { fixSafariScrollSnap } from './scoll-snap'
-import { AdjustVRController } from './adjust-vr'
-import { ScrollSnapMandatoryController } from './scroll-snap-mandatory'
+import { setupHasScrolled } from './js/has-scrolled'
+import { fixSafariScrollSnap } from './js/scoll-snap'
+import { AdjustVRController } from './js/adjust-vr'
+import { ScrollSnapMandatoryController } from './js/scroll-snap-mandatory'
 
 // vekApp.register('can-scroll', CanScrollController)
 // vekApp.register('scroll-to', ScrollToController)
