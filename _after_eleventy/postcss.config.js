a4e1472d1f18b6c1174efd6bedabfff1a7eae898
Julie Koubová <julie@koubova.net>
2020-04-16 01:04:02 +0200

    purge css

---
+++ _after_eleventy/postcss.config.js
@@ -0,0 +1,22 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const { resolve } = require('path')
+
+const site = resolve(__dirname, '../_site/**/*.html')
+
+const plugins = [
+  cssnano({ preset: 'default' }),
+  purgecss({
+    content: [site],
+    extractors: [
+      {
+        extractor(content) {
+          return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+        },
+        extensions: ['css', 'html', 'js'],
+      },
+    ]
+  })
+]
+
+module.exports = { plugins }
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss')('./tailwind.config.js')
-  ]
-}
+const autoprefixer = require('autoprefixer')
+const tailwindcss = require('tailwindcss')
+
+const plugins = [
+  autoprefixer,
+  tailwindcss('./tailwind.config.js'),
+]
+
+module.exports = { plugins }
