4ce1b3f94121da2413f6bab0e5b83c60afa4ed5b
Julie Koubová <julie@koubova.net>
2020-04-19 23:19:14 +0200

    deploy

---
+++ _scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+if [ -z "$DEPLOY_HOST" ]; then
+  echo 'Action did not find the DEPLOY_HOST variable.' >2
+  exit 1
+fi
+
+if [ -z "$DEPLOY_KEY" ]; then
+  echo 'Action did not find the DEPLOY_KEY secret.' >2
+  exit 1
+fi
+
+if [ -z "$DEPLOY_KNOWN_HOSTS" ]; then
+  echo 'Action did not find the DEPLOY_KNOWN_HOSTS secret.' >2
+  exit 1
+fi
+
+SSH_PATH="$HOME/.ssh"
+SSH_OPTS="-i \"$SSH_PATH/deploy_key\""
+
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/deploy_key"
+touch "$SSH_PATH/known_hosts"
+
+chmod 700 "$SSH_PATH"
+chmod 706 "$SSH_PATH/known_hosts" "$SSH_PATH/deploy_key"
+
+printf '%b\n' "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
+printf '\n%s\n' "$DEPLOY_KNOWN_HOSTS" >> "$SSH_PATH/known_hosts"
+
+exec rsync -avz --checksum --delete-after \
+  -e "ssh $SSH_OPTS" \
+  "$GITHUB_WORKSPACE/$1/" \
+  "$DEPLOY_HOST"
