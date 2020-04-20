1ba72452cba71bb173d48a3efa9c0e60ebf49f53
Julie Koubová <julie@koubova.net>
2020-04-20 18:41:42 +0200

    cleanup

--- packages/resizer/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "stimulus";
-export declare abstract class ResizerController extends Controller {
-    private readonly handler;
-    connect(): void;
-    disconnect(): void;
-    abstract resized(): void;
-}
