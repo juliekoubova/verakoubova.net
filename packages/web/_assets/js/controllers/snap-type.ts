e5f12025856daf66f556865e98d2935789a3828c
Julie Koubová <julie@koubova.net>
2020-04-21 23:53:28 +0200

    no snap on home

--- packages/web/_assets/js/controllers/snap-type.ts
+++ packages/web/_assets/js/controllers/snap-type.ts
@@ -1,4 +1,3 @@
-import { makeResizer } from '@verakoubova/stimulus'
 import { Controller } from 'stimulus'
 
 const isSafari =
@@ -11,19 +10,9 @@ function toggleScrollSnapClass(className: string, value?: boolean) {
 }
 
 export class SnapTypeController extends Controller {
-  static targets = ["determineBy"]
-  readonly hasDetermineByTarget!: boolean
-  readonly determineByTarget?: Element
-
-  initialize() {
-    makeResizer(this, () => {
-      const target = this.hasDetermineByTarget
-        ? this.determineByTarget!
-        : this.element
-      const { height } = target.getBoundingClientRect()
-      const mandatory = this.data.has('mandatory') && height <= innerHeight
-      toggleScrollSnapClass('snap-y-mandatory', mandatory)
-      toggleScrollSnapClass('snap-y-proximity', !mandatory)
-    })
+  connect() {
+    const mandatory = this.data.has('mandatory')
+    toggleScrollSnapClass('snap-y-mandatory', mandatory)
+    toggleScrollSnapClass('snap-y-proximity', !mandatory)
   }
 }
