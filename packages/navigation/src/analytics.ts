3cbde0a61fc7a9f578aa2456be1ce70a0d356132
Julie Koubová <julie@koubova.net>
2020-05-05 22:03:30 +0200

    analytics.js

--- packages/navigation/src/analytics.ts
+++ packages/navigation/src/analytics.ts
@@ -1,11 +1,13 @@
 import { currentPosition } from "./position";
 
 declare global {
-  function vekPageView(): void
+  function ga(command: 'send', event: 'pageview'): void
+  function ga(command: 'set', field: 'location', value: string): void
 }
 
 export function setupScrollAnalytics() {
-  if (vekPageView) {
-    currentPosition.subscribe(() => vekPageView())
-  }
+  currentPosition.subscribe(() => {
+    ga('set', 'location', location.href)
+    ga('send', 'pageview')
+  })
 }
