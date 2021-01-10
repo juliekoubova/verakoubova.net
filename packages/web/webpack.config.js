127bf1b668c89beb0690e2ab0f30c16e663dbbe3
Julie Koubova <jkoubova@microsoft.com>
2021-01-10 14:18:27 +0100

    legacy bundle

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -7,34 +7,63 @@ const absolute = resolve.bind(undefined, __dirname)
 const assets = absolute.bind(undefined, '_assets')
 const site = absolute.bind(undefined, '_site')
 
-/** @type {import('webpack').Configuration} */
-const config = {
-  devtool: 'source-map',
-  entry: {
-    main: assets('main.js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      }, {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ['source-map-loader'],
-      }, {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  output: {
-    path: site()
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-  ]
+
+/**
+ * @param {import('@babel/preset-env').TargetsOptions} targets
+ * @returns {import('@babel/preset-env').Options}
+ */
+function babelPresetEnvOptions(targets) {
+  return {
+    corejs: 3,
+    modules: false,
+    targets,
+    useBuiltIns: 'usage',
+  }
 }
 
-module.exports = config
+/**
+ * @param {string} filename
+ * @param {import('@babel/preset-env').TargetsOptions} targets
+ * @returns {import('webpack').Configuration}
+ */
+function createConfig(filename, targets = undefined) {
+  return {
+    devtool: 'source-map',
+    entry: {
+      main: assets('main.js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        }, {
+          test: /\.js$/,
+          enforce: 'pre',
+          loader: 'source-map-loader',
+        }, {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', babelPresetEnvOptions(targets)]
+            ]
+          }
+        }
+      ]
+    },
+    output: {
+      filename,
+      path: site(),
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+    ]
+  }
+}
+
+module.exports = [
+  createConfig('main.es5.js', { browsers: 'defaults' }),
+  createConfig('main.esm.js', { esmodules: true })
+]
