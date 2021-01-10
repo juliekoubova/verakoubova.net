fc4dcb619e040b55cf5e7de9dfea38583772650c
Julie Koubova <jkoubova@microsoft.com>
2021-01-10 14:44:11 +0100

    update to turbo

--- packages/web/_assets/js/turbolinks.js
+++ packages/web/_assets/js/turbolinks.js
@@ -1,7 +1,5 @@
-import { start } from 'turbolinks'
-
-start()
-addEventListener('turbolinks:load', () => {
+import '@hotwired/turbo'
+addEventListener('turbo:load', () => {
   const meta = document.querySelector('meta[http-equiv=content-language]')
   const language = meta?.getAttribute('content')
   document.documentElement.setAttribute('lang', language || '')
